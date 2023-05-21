44bf8a187f1b006c56482328dee383be99d6f55d
Uzair Abdullah <uzairabdullah973@gmail.com>
2023-05-21 20:01:42 +0500

    added catch in case of fail

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
 	<Router>
 		<App />
