1ff4dddec6c836697de1b138765b946a13005a35
Hassan <mr.und3ad1@gmail.com>
2023-05-23 17:24:28 +0500

    hassan

--- src/pages/terms-and-conditions/TermsAndConditions.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Terms from "../../components/terms/Terms";
-
-const TermsAndConditions = ({ isMobile }) => {
-	return <Terms isMobile={isMobile} />;
-};
-
-export default TermsAndConditions;
